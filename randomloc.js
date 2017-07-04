f83bc23331c3f51c77b4359628b948610b0b6352
dpatel1996 <dpatel108@hawk.iit.edu>
2017-07-04 11:47:16 -0500

    trying different
    Trying to do something different playing around

--- randomloc.js
+++ randomloc.js
@@ -1,4 +1,4 @@
-var map;
+/*var map;
   funtion initMap() 
   {
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -7,20 +7,19 @@ var map;
       zoom: 12
     });
     
-    var mark_skydeck= new google.maps.Marker({
+    var markskydeck= new google.maps.Marker({
       position: {lat: 41.878741, lng:-87.635943},
       map:map,
-      title: 'Willis Tower Chicago',
+      title: 'WillisTower',
       icon : 'images/skydeck_chi.jpg',
       animation: google.maps.Animation.BOUNCE,
     })
-    var detail ='<h1> Willis Tower </h1> <p> skydeck is the tallest building of the chicago located in downtown and it is the one of the tourist attraction spot</p>';
     
     var skydeck = new google.maps.InfoWindow({
-      content: detail
+      content: '<h1> Willis Tower </h1> <p> skydeck is the tallest building of the chicago located in downtown and it is the one of the tourist attraction spot</p>';
     });
-      mark_skydeck.addListener('click',funtion(){
-      skydeck.open(map,mark_skydeck);
+      markskydeck.addListener('click',funtion(){
+      skydeck.open(map, markskydeck);
       
       setTimeout(funtion() {
       skydeck.close();}, 12000);
@@ -28,11 +27,34 @@ var map;
 
     
    
-    google.maps.event.addDomListener(window,'load',init);
-  }
+    google.maps.event.addDomListener(window,'load',initialize);
+  }*/
   /*lat: 21.705136, lng: 72.995875*/
   
   /* Navy Pier lat: 41.891551 lng: -87.607375 
   
      Millenium Park: 41.882702 lng: -87.619394 */
      
+function initMap(){
+  
+  var chicago = new google.maps.LatLng(41.875672,-87.624347);
+  
+  var skydeck = new google.maps.LatLng(41.878741, -87.635943);
+  
+  var navyPier = new google.maps.LatLng(41.891551, -87.607375);
+  
+  var MilleniumPark = new google.maps.LatLng(41.882702, -87.619394);
+  
+  var mapId= document.getElementById("map");
+  var mapCho = { center: chicago, zoom: 14};
+  var map: new.google.maps.Map(mapId,mapCho);
+  
+  var roadPath = new google.maps.Polygon({
+    direction: [skydeck, navyPier, MilleniumPark],
+    strokeColor: "#BEEC78",
+    strokeOpacity: 1.0,
+    strokeWeight: 3;
+  })
+  roadPath.setMap(map);
+}
+
